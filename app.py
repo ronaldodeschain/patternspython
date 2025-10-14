40227f0862f8445812b981990b90798d7dab46e3
RONALDO LEMOS DA SILVA <01300771003@senacrs.edu.br>
2025-10-13 21:38:20 -0300

    finalizando por agora

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 atendentes = []
 
@@ -6,12 +7,39 @@ def adicionar_atendente():
     nome = entrada_nome.get().strip()
 
     if not nome:
-        #pendente
+        messagebox.showwarning("Nome vazio","Digite um nome svp.")
         return 
     
     if nome in [a["nome"] for a in atendentes]:
-        #pendente
+        messagebox.showinfo("Duplicado","Atendente já existe.")
         return
+    
+    atendentes.append({"nome":nome,"vendas":0})
+    entrada_nome.delete(0,tk.END)
+
+def resetar_atendentes():
+    if messagebox.askyesno("Resetar","Deseja resetar os dados de atendentes?"):
+        atendentes.clear()
+    
+def incrementar_vendas(indice):
+    atendentes[indice]["vendas"] += 1
+
+def atualizar_interface():
+    for widget in quadro_atendentes.winfo_children():
+        widget.destroy()
+
+    for i,atendente in enumerate(atendentes):
+        texto = f"{atendente['nome']} - Vendas: {atendente['vendas']}"
+        rotulo = tk.Label(quadro_atendentes,text=texto)
+        rotulo.grid(row=i,column=0,sticky="w")
+
+        botao_incrementar = tk.Button(
+            quadro_atendentes,
+            text="+1",
+            command=lambda indice=i: incrementar_vendas(indice)
+        )
+        botao_incrementar.grid(row=i,column=1)
+
 
 #Interface Principal
 janela = tk.Tk()
@@ -19,5 +47,14 @@ janela.title("Controle de Vendas - Smart View")
 
 entrada_nome = tk.Entry(janela)
 entrada_nome.pack(pady=5,padx=15)
+
+botao_adicionar = tk.Button(janela, text="Adicionar Atendente", command=adicionar_atendente)
+botao_adicionar.pack(pady=5,padx=15)
+
+botao_resetar = tk.Button(janela,text="Resetar",command=resetar_atendentes)
+botao_resetar.pack(pady=5,padx=15)
+
+quadro_atendentes = tk.Frame(janela)
+quadro_atendentes.pack(pady=10,padx=10)
 
 janela.mainloop()
